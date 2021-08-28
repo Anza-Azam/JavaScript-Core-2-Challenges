e2ab90a20fc14f26ba49c9c2b814a913923493f6
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-08-27 17:25:30 -0700

    updated proj

--- challenge-guess-the-number/script.js
+++ challenge-guess-the-number/script.js
@@ -1,39 +1,40 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
-
-
+let maxTries = 3;
+let tries = 0;
 console.log(randomNumber)
-function guessNumber() {
-  //Collect input from the user
-  let guess = document.querySelector('.inputs-Values').value
+function guessNumber() { // Collect input from the user
+    let guess = document.querySelector('.inputs-Values').value
+    tries += 1
+    console.log(maxTries)
+    if (maxTries === tries) {
 
-  //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
-  if (guess <= 0 || guess > 100) {
-    let outPut = document.querySelector('.final-output')
-    outPut.value = 'Please enter a number between 1 and 100'
+        let outPut = document.querySelector('.final-output')
+        outPut.value = 'You Lose, the number was ' + randomNumber;
 
-  }
+    }
+    // If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100" else if (guess <= 0 || guess > 100) {
+        let outPut = document.querySelector('.final-output')
+        outPut.value = 'Please enter a number between 1 and 100'
+
+    }
 
 
-  //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
-  else if (guess > randomNumber) {
-    let outPut = document.querySelector('.final-output')
-    outPut.value = 'Number is too high';
-  }
-  //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
-else if (guess < randomNumber) {
-  let outPut = document.querySelector('.final-output'
-)
-  outPut.value = 'Number is too low';
-}
+    // If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print) else if (guess > randomNumber) {
+        let outPut = document.querySelector('.final-output')
+        outPut.value = 'Number is too high';
+    }
+    // If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print) else if (guess < randomNumber) {
+        let outPut = document.querySelector('.final-output')
+        outPut.value = 'Number is too low';
+    }
 
-  //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
-//let guess = document.querySelector('.inputs-Values').value
- // (!(randomNumber<guess)&&!(randomNumber>guess)) 
- else {  console.log(guess)
-  let outPut = document.querySelector('.final-output')
-    outPut.value = 'Guess is correct. You win!'
 
-}
+    // If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!" else {
+        console.log(guess)
+        let outPut = document.querySelector('.final-output')
+        outPut.value = 'Guess is correct. You win!' + 'It took you ' + tries + ' tries to win'
+
+    }
 
 }
 
@@ -43,24 +44,28 @@ else if (guess < randomNumber) {
 // 1. Reset the values inside the body of the function
 // 2. Attach our new game button using an event listener to the .btnNewGame button
 function newGame() {
-  //Your code here
-  //Reset randomNumber
-  randomNumber = Math.floor(Math.random() * 100 + 1)
-console.log(randomNumber)
-  //Reset users input field
-  let guess = document.getElementById('num')
+    // Your code here
+    // Reset randomNumber
+    randomNumber = Math.floor(Math.random() * 100 + 1)
+    console.log(randomNumber)
+    // Reset users input field
+    let guess = document.getElementById('num')
+    tries = 0;
+    maxTries = 3;
+    guess.value = '';
+    let outPut = document.querySelector('.final-output')
+    outPut.value = ' '
 
-  guess.value='';
-console.log(guess)
+    console.log(guess)
 
-  //Reset tries, and triesTaken by the user
+    // Reset tries, and triesTaken by the user
 }
 
-//keyboard exception
+// keyboard exception
 function keyBoardEvents(e) {
-  if (e.keyCode === 13) {
-    guessNumber();
-  }
+    if (e.keyCode === 13) {
+        guessNumber();
+    }
 }
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
