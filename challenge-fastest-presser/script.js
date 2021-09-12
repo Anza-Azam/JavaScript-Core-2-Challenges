1b6def1eb6d37de3163eb300d80488ae4eaf3b52
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-12 22:52:22 +0100

    feat: fastest-presses

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,147 +1,124 @@
 let counterS;
 let counterL;
-function setup() { 
-  
-  const startButton = document.getElementById('start');
-  startButton.addEventListener('click', startGame)
-  document.removeEventListener("keypress", keyBoardEvents)
-  const confetti = document.getElementById('my-canvas')
-  confetti.style.display = 'none';
-  const confetti2 = document.getElementById('my-canvas2')
-  confetti2.style.display='none'
-  
-
-
+function setup() {
+	const startButton = document.getElementById('start');
+	startButton.addEventListener('click', startGame);
+	document.removeEventListener('keypress', keyBoardEvents);
+	const confetti = document.getElementById('my-canvas');
+	confetti.style.display = 'none';
+	const confetti2 = document.getElementById('my-canvas2');
+	confetti2.style.display = 'none';
 }
-
 
 function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
-    // On 'S' Pressed
-    counterS += 1;
-    const players = document.getElementById("s");
-    console.log(counterS)
-     players.innerText = counterS;
-
-  } else if (e.keyCode === 76) {
-    // On 'L' Pressed
-    counterL += 1;
-     const playerL = document.getElementById("l");
-    playerL.innerText = counterL;
-    console.log(counterL)
-  }
+	if (e.keyCode === 115) {
+		// On 'S' Pressed
+		counterS += 1;
+		const players = document.getElementById('s');
+		console.log(counterS);
+		players.innerText = counterS;
+	} else if (e.keyCode === 108) {
+		// On 'L' Pressed
+		counterL += 1;
+		const playerL = document.getElementById('l');
+		playerL.innerText = counterL;
+		console.log(counterL);
+	}
 }
 
-  let interval
+let interval;
 
-function startGame()
-{
-counterS = 0;
-  counterL = 0;
-  const confetti = document.getElementById('my-canvas')
-  confetti.style.display = 'none';
-  const confetti2 = document.getElementById('my-canvas2')
-  confetti2.style.display='none'
-  const timeLimit = document.getElementById("timer");
-  document.addEventListener("keypress", keyBoardEvents);
-  if (timeLimit.value === '') {
-    alert('please input time')
-    restartButton();
-  }
-  console.log(timeLimit.value);
-  const player = document.getElementById('result')
-  player.innerText = timeLimit.value;
-  interval = setInterval(checkTime, 1000);
-
-
+function startGame() {
+	counterS = 0;
+	counterL = 0;
+	const confetti = document.getElementById('my-canvas');
+	confetti.style.display = 'none';
+	const confetti2 = document.getElementById('my-canvas2');
+	confetti2.style.display = 'none';
+	const timeLimit = document.getElementById('timer');
+	document.addEventListener('keypress', keyBoardEvents);
+	if (timeLimit.value === '') {
+		alert('please input time');
+		restartButton();
+	}
+	console.log(timeLimit.value);
+	const player = document.getElementById('result');
+	player.innerText = timeLimit.value;
+	interval = setInterval(checkTime, 1000);
 }
 
 function checkTime() {
-    const player = document.getElementById('result')
-  const timeLimit = document.getElementById("timer");
-  if (timeLimit.value < 1) {
-    clearInterval(interval)
-    stopgame();
-  }
-  else {
-     player.innerText = timeLimit.value-1;
-    (timeLimit.value -= 1);
-   
-  
-  }
+	const player = document.getElementById('result');
+	const timeLimit = document.getElementById('timer');
+	if (timeLimit.value < 1) {
+		clearInterval(interval);
+		stopgame();
+	} else {
+		player.innerText = timeLimit.value - 1;
+		timeLimit.value -= 1;
+	}
 }
 window.onload = setup;
 
 function stopgame() {
-  clearInterval(interval)
-   
-  const player = document.getElementById('result')
-  if (counterS === counterL && counterS > 0 && counterL > 0) {
-    player.innerText = "game tied, new game starts in 4 seconds"
-       
-  }
-  else if (counterS > counterL)
-  {
-    (player.innerText = "Player S wins the game" + " new game starts in 4 seconds")
-   // const winner= document.createElement('canvas')
-    //winner.id='my-canvas2';
-    var ele = document.getElementById('my-canvas2');
-   // ele.appendChild(winner);
-ele.style.display='flex'
-    var confettiSettings = { target: 'my-canvas2' };
-    var confetti = new ConfettiGenerator(confettiSettings);
-    confetti.render();
-  }
-  
-  else if (counterL > counterS) {
+	clearInterval(interval);
 
-    player.innerText = "Player L wins the game" + " new game starts in 4 seconds"
-   // const winner= document.createElement('canvas')
-    //winner.id='my-canvas';
-    var ele = document.getElementById('my-canvas');
-    //ele.appendChild(winner);
-    ele.style.display = 'flex';
-    var confettiSettings = { target: 'my-canvas' };
-    var confetti = new ConfettiGenerator(confettiSettings);
-    confetti.render();
-  }
-    else { player.innerText= 'No Winners' + ' new game starts in 4 seconds' }
-  
+	const player = document.getElementById('result');
+	if (counterS === counterL && counterS > 0 && counterL > 0) {
+		player.innerText = 'game tied, new game starts in 4 seconds';
+	} else if (counterS > counterL) {
+		player.innerText = 'Player S wins the game' + ' new game starts in 4 seconds';
+		let element = document.getElementById('my-canvas2');
 
-  setTimeout(() => {
-    const timeLimit = document.getElementById("timer");
-    const players = document.getElementById("s");
-     const playerL= document.getElementById("l");
-    // const timeLimit = document.getElementById("timer");
-    player.innerText = ''
-    players.innerText = 'Press "S"'
-    playerL.innerText = 'Press "L"'
-    const confetti = document.getElementById('my-canvas')
-  confetti.style.display = 'none';
-  const confetti2 = document.getElementById('my-canvas2')
-  confetti2.style.display='none'
-    timeLimit.value=''
-  },4000);
- // player.innerText = 'game stopped'
-  
+		element.style.display = 'flex';
+		let confettiSettings = { target: 'my-canvas2' };
+		let confetti = new ConfettiGenerator(confettiSettings);
+		confetti.render();
+	} else if (counterL > counterS) {
+		player.innerText = 'Player L wins the game' + ' new game starts in 4 seconds';
+
+		let element = document.getElementById('my-canvas');
+
+		element.style.display = 'flex';
+		let confettiSettings = { target: 'my-canvas' };
+		let confetti = new ConfettiGenerator(confettiSettings);
+		confetti.render();
+	} else {
+		player.innerText = 'No Winners' + ' new game starts in 4 seconds';
+	}
+
+	setTimeout(() => {
+		const timeLimit = document.getElementById('timer');
+		const players = document.getElementById('s');
+		const playerL = document.getElementById('l');
+document.removeEventListener('keypress', keyBoardEvents);
+		player.innerText = '';
+		players.innerText = 'Press "S"';
+		playerL.innerText = 'Press "L"';
+		const confetti = document.getElementById('my-canvas');
+		confetti.style.display = 'none';
+		const confetti2 = document.getElementById('my-canvas2');
+		confetti2.style.display = 'none';
+		timeLimit.value = '';
+	}, 4000);
 }
- 
+
 const restart = document.getElementById('restart');
-restart.addEventListener('click', restartButton)
+restart.addEventListener('click', restartButton);
 function restartButton() {
-  clearInterval(interval)
-  const confetti = document.getElementById('my-canvas')
-  confetti.style.display = 'none';
-  const confetti2 = document.getElementById('my-canvas2')
-  confetti2.style.display='none'
-  const player = document.getElementById('result')
-  const timeLimit = document.getElementById("timer");
-  const players = document.getElementById("s");
-  const playerL = document.getElementById("l");
-  // const timeLimit = document.getElementById("timer");
-  player.innerText = ''
-  players.innerText = 'Press "S"'
-  playerL.innerText = 'Press "L"'
-    
-  timeLimit.value = ''
+	clearInterval(interval);
+	const confetti = document.getElementById('my-canvas');
+	confetti.style.display = 'none';
+	const confetti2 = document.getElementById('my-canvas2');
+	confetti2.style.display = 'none';
+	const player = document.getElementById('result');
+	const timeLimit = document.getElementById('timer');
+	const players = document.getElementById('s');
+	const playerL = document.getElementById('l');
+
+	player.innerText = '';
+	players.innerText = 'Press "S"';
+	playerL.innerText = 'Press "L"';
+
+	timeLimit.value = '';
 }
